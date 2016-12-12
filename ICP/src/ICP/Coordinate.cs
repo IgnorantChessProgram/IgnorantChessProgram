35390344abae58d6ef95d246da930be6a0e19c36
Ignorant Chess Programmer <>
2016-12-11 21:27:06 -0700

    Start project as a .NET Core class library.  Added structs for coordinates and moves

---
+++ ICP/src/ICP/Coordinate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICP
+{
+    public struct Coordinate
+    {
+        public enum FileLetter
+        {
+            a = 1,
+            b,
+            c,
+            d,
+            e,
+            f,
+            g,
+            h
+        }
+
+        public int Rank { get; }
+        public FileLetter File { get; }
+
+        public Coordinate(int rank, int file)
+        {
+            if (rank < 1 || rank > 8 || file < 0 || file > 8)
+            {
+                throw new ArgumentException($"Invalid coordinate: {file},{rank}");
+            }
+
+            Rank = rank;
+            File = (FileLetter)file;
+        }
+        public Coordinate(int rank, FileLetter file) : this(rank, (int) file)
+        {
+        }
+
+        public override string ToString()
+        {
+            return File.ToString() + Rank;
+        }
+    }
+}
---
+++ ICP/src/ICP/Move.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ICP
+{
+    public struct Move
+    {
+        public Coordinate StartCoordinate { get; }
+        public Coordinate EndCoordinate { get; }
+
+        public TimeSpan TimeLeft { get; }
+
+        public Move(Coordinate startCoordinate, Coordinate endCoordinate, TimeSpan timeLeft)
+        {
+            StartCoordinate = startCoordinate;
+            EndCoordinate = endCoordinate;
+            TimeLeft = timeLeft;
+        }
+    }
+}
