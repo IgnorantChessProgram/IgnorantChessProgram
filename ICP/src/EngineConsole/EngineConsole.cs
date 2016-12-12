172fcdd718359e7ee54fb0b7ec0b67ad0bdd00ac
Ignorant Chess Programmer <>
2016-12-11 23:04:53 -0700

    Forgot to add the files

---
+++ ICP/src/EngineConsole/EngineConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ICP;
+
+namespace EngineConsole
+{
+    public class EngineConsole
+    {
+        public static void Main(string[] args)
+        {
+            Board board = new Board();
+            Console.Write(board.DebugString());
+            Console.ReadLine();
+        }
+    }
+}
